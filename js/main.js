b1d7abcfd1cd077615930fed9a114bb1822482a0
ThuyTiencodelight <tienht@codelight.co>
2022-11-16 14:59:51 +0700

    style: add popup move to other site

--- js/main.js
+++ js/main.js
@@ -26,5 +26,5 @@ $(document).ready(function(){
           $("body").find(".nav__menu").removeClass("visible");
         }
     });
-
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('NotificationModal')).show();
 });
